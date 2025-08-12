aaa9c0b9d6dd982eae9d31fbd4daa02341d571d4
tb-software <151437619+tb-software@users.noreply.github.com>
2025-08-12 17:53:04 +0200

    Refactor simulator tests

--- EncoderWpfApp.Tests/HardwareSimulatorWindowTests.vb
+++ EncoderWpfApp.Tests/HardwareSimulatorWindowTests.vb
@@ -8,7 +8,6 @@ Imports System.Collections.Generic
 Imports System.Threading.Tasks
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 Imports EncoderLib
-Imports EncoderWpfApp
 
 <TestClass>
 Public Class HardwareSimulatorWindowTests
@@ -16,23 +15,23 @@ Public Class HardwareSimulatorWindowTests
     Public Async Function ClickAllButtonsAsync_SendsExpectedSequences() As Task
         Dim recorder As New RecordingKeyboardSender()
         Dim processor As New EncoderInputProcessor(recorder)
-        Dim window As New HardwareSimulatorWindow(processor, Function(ts) Task.CompletedTask)
+        Dim window As New EncoderWpfApp.HardwareSimulatorWindow(processor, Function(ts) Task.CompletedTask)
 
         Await window.ClickAllButtonsAsync()
         Await Task.Delay(300)
 
         Dim expected = {"Up", "Down", "PageUp", "PageDown", "Escape"}
-        Dim actual = recorder.Keys.ConvertAll(Function(k) String.Join("+", k)).ToArray()
+        Dim actual = recorder.SentKeySequences.ConvertAll(Function(k) String.Join("+", k)).ToArray()
         CollectionAssert.AreEqual(expected, actual)
     End Function
 
     Private Class RecordingKeyboardSender
         Implements IKeyboardSender
 
-        Public ReadOnly Keys As New List(Of IReadOnlyList(Of WindowsKey))()
+        Public ReadOnly SentKeySequences As New List(Of IReadOnlyList(Of WindowsKey))()
 
-        Public Sub SendKeys(keys As IReadOnlyList(Of WindowsKey)) Implements IKeyboardSender.SendKeys
-            Keys.Add(keys)
+        Public Sub SendKeys(keySequence As IReadOnlyList(Of WindowsKey)) Implements IKeyboardSender.SendKeys
+            Me.SentKeySequences.Add(keySequence)
         End Sub
     End Class
 End Class
